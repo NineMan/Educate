5345df3b45e697976a59625cae26e29f4098a3ff
NineMan <nelubinmv@inbox.ru>
2020-04-27 03:55:39 +0400

    Create Content create Content and 4 related models (Text, File, Image, Video)

--- educa/courses/models.py
+++ educa/courses/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
 
 
 class Subject(models.Model):
@@ -35,4 +37,46 @@ class Module(models.Model):
     description = models.TextField(blank=True)
 
     def __str__(self):
-    	return self.title
+        return self.title
+
+
+class Content(models.Model):
+    module = models.ForeignKey(Module, on_delete=models.DO_NOTHING, related_name='contents')
+    content_type = models.ForeignKey(ContentType,
+                                     on_delete=models.DO_NOTHING,
+                                     limit_choices_to={'model__in': ('text',
+                                                                     'video',
+                                                                     'image',
+                                                                     'file')})
+    object_id = models.PositiveIntegerField()
+    item = GenericForeignKey('content_type', 'object_id')
+
+
+class ItemBase(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.DO_NOTHING, related_name='%(class)s_related')
+    title = models.CharField(max_length=200)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return self.title
+
+
+class Text(ItemBase):
+    content = models.TextField()
+
+
+class File(ItemBase):
+    content = models.FileField(upload_to='files')
+
+
+class Image(ItemBase):
+    content = models.FileField(upload_to='images')
+
+
+class Video(ItemBase):
+    models.URLField()
+
